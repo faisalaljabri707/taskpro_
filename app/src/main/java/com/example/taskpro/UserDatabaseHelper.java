ad55cb4b85288368ed6c38a0c7f3508380743c7b
faisa <faisal707100@gmail.com>
2024-01-01 02:50:23 +0400

    Initial commit

---
+++ app/src/main/java/com/example/taskpro/UserDatabaseHelper.java
@@ -0,0 +1,74 @@
+package com.example.taskpro;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class UserDatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "user_database.db";
+    private static final int DATABASE_VERSION = 1;
+
+    // Table and Column Names
+    private static final String TABLE_USERS = "users";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_USERNAME = "username";
+    private static final String COLUMN_PASSWORD = "password";
+
+    public UserDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Create the users table
+        String createTableQuery = "CREATE TABLE " + TABLE_USERS + " ("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COLUMN_USERNAME + " TEXT, "
+                + COLUMN_PASSWORD + " TEXT)";
+        db.execSQL(createTableQuery);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Handle database upgrades (if needed)
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERS);
+        onCreate(db);
+    }
+
+    // Method to add a new user to the database
+    public long addUser(String username, String password) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_USERNAME, username);
+        values.put(COLUMN_PASSWORD, password);
+        long newRowId = db.insert(TABLE_USERS, null, values);
+        db.close();
+        return newRowId;
+    }
+
+    // Method to check if a user with the given username exists
+    public boolean checkUserExists(String username) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(TABLE_USERS, null, COLUMN_USERNAME + "=?",
+                new String[]{username}, null, null, null);
+        boolean exists = cursor.getCount() > 0;
+        cursor.close();
+        db.close();
+        return exists;
+    }
+
+    // Method to check if the given username and password match a user in the database
+    public boolean checkUserCredentials(String username, String password) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(TABLE_USERS, null,
+                COLUMN_USERNAME + "=? AND " + COLUMN_PASSWORD + "=?",
+                new String[]{username, password}, null, null, null);
+        boolean isValid = cursor.getCount() > 0;
+        cursor.close();
+        db.close();
+        return isValid;
+    }
+}
+
